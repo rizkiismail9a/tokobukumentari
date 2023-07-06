d10103ddd072b0576f291299ba3d219f06cbca81
rizkiismail <laksmimentari@gmail.com>
2023-07-06 14:21:35 +0700

    add icons and popUp

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,15 @@
 const hamburgerBtn = document.getElementById("hamburger-btn");
 const navbarList = document.querySelector(".navbar__list");
+const popUp = document.getElementById("popUp");
+const popUpClose = document.getElementById("popUpClose");
 
 hamburgerBtn.addEventListener("click", () => {
   navbarList.classList.toggle("hamburger-toggle");
 });
+const showDetail = () => {
+  document.querySelector(".popUp").style.top = "0";
+};
+
+const closeDetail = () => {
+  document.querySelector(".popUp").style.top = "-1000px";
+};
